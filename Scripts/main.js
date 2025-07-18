c7d446174f5ed40915c42e0232aef15cfe62e419
Abhinav-Sharmaaaa <abhinav2654@outlook.com>
2025-07-18 20:09:27 +0530

    file structure updated

--- Scripts/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('mousemove', (e) => {
-    const cursorGlow = document.querySelector('.cursor-glow');
-
-    cursorGlow.style.left = `${e.clientX}px`;
-    cursorGlow.style.top = `${e.clientY}px`;
-});
